69e1c9eb012aae77c4e75926a9a38826328e96cb
random_oracle <ra728@york.ac.uk>
2014-06-14 00:04:55 +0100

    Game now displays a window.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,21 @@
 package main;
 
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsEnvironment;
+import java.awt.Insets;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
 /**
  * Creates an instance of the game, and handles it's execution.
  */
 public class Main {
 	
-	/** The possible game statuses
+	/** The possible game statuses.
 	 * <p>
 	 * <table>
 	 * <thead>Valid statuses:</thead>
@@ -22,7 +32,17 @@ public class Main {
 	/** The current game instance */
 	private static Main instance;
 	
-	/** The game's status */
+	/** The full-screen toggle.
+	 * <p>
+	 * If this is true, the game <b>can</b> run in full-screen mode
+	 * (provided that other conditions are met).
+	 * <br>
+	 * If this is false, the game <b>will not</b> run in full-screen mode.
+	 * </p>
+	 */
+	private static final boolean fullscreenToggle = true;
+	
+	/** The game's current status */
 	private Status gameStatus;
 	
 	/** The monitor used to block the control thread until the splash ends */
@@ -44,25 +64,30 @@ public class Main {
 	private Main() {
 		// Start by playing the splash screen
 		gameStatus = Status.SPLASHING;
-		splash(); // Splash happens asynchronously, so this will not block
+		splash();
+		
+		// Load game scenes and resources
+		// This occurs concurrently to the splash screen
 		//TODO: Load scenes and resources
 		
 		// Wait for the splash screen to finish
-		while (gameStatus == Status.SPLASHING) {
-			try {
-				splashMonitor.wait();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+		synchronized (splashMonitor) {
+			while (gameStatus == Status.SPLASHING) {
+				try {
+					splashMonitor.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 		
-		// Then set up the game window
+		// Set up the game window
 		gameStatus = Status.WAITING;
 		setUpWindow();
 		gameStatus = Status.RUNNING;
 		
 		// Do the update and render loop
-		while (gameStatus == Status.RUNNING) {
+		while (gameStatus == Status.RUNNING && !Display.isCloseRequested()) {
 			update();
 			render();
 		}
@@ -89,8 +114,10 @@ public class Main {
 				
 				// End by signalling that the main control thread
 				// can proceed
-				gameStatus = Status.WAITING;
-				splashMonitor.notifyAll();
+				synchronized (splashMonitor) {
+					gameStatus = Status.WAITING;
+					splashMonitor.notifyAll();
+				}
 			}
 		});
 		
@@ -100,9 +127,58 @@ public class Main {
 	
 	/**
 	 * Sets up the game window.
+	 * @param fullscreen - <tt>true</tt> if the game should be presented
+	 * 			full-screen
 	 */
 	private void setUpWindow() {
-		//TODO
+		// Check if the screen supports full-screen
+		boolean fullscreenEnabled =
+				Display
+				.getDisplayMode()
+				.isFullscreenCapable();
+		
+		try {
+			// Set the display mode
+			if (fullscreenEnabled && fullscreenToggle) {
+				Display.setFullscreen(true);
+			} else {
+				// Work out the fall-back screen size (for cases where
+				// full-screen fails)
+				GraphicsConfiguration conf =
+						GraphicsEnvironment
+						.getLocalGraphicsEnvironment()
+						.getDefaultScreenDevice()
+						.getDefaultConfiguration();
+				
+				// Get window offsets
+				Insets insets =
+						Toolkit
+						.getDefaultToolkit()
+						.getScreenInsets(conf);
+				
+				// Get screen dimensions
+				Rectangle bounds = conf.getBounds();
+				
+				// Get the available screen space: dimensions - insets
+				// The x and y alterations are not necessary, but are provided
+				// for completeness
+				bounds.x += insets.left;
+				bounds.y += insets.top;
+				bounds.width -= (insets.left + insets.right);
+				bounds.height -= (insets.top + insets.bottom);
+				
+				Display.setDisplayMode(
+						new DisplayMode(bounds.width - 40, bounds.height - 50)
+				);
+				
+				Display.setLocation(bounds.x + 20, bounds.y + 10);
+			}
+			
+			// Create the display
+			Display.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	/**
@@ -116,13 +192,17 @@ public class Main {
 	 * Renders graphical elements.
 	 */
 	private void render() {
-		//TODO
+		// Update the display
+		Display.update();
 	}
 	
 	/**
 	 * Handles game exit.
 	 */
 	public void exit() {
+		// Close the window
+		Display.destroy();
+		
 		// Exit the game
 		System.exit(0);
 	}
@@ -132,7 +212,7 @@ public class Main {
 	 * Gets the current game instance.
 	 * @return the current game instance
 	 */
-	public Main instance() {
+	public static Main instance() {
 		return instance;
 	}
 	
@@ -150,8 +230,8 @@ public class Main {
 	 * </p>
 	 * @return the game's status
 	 */
-	public Status gameStatus() {
-		return gameStatus;
+	public static Status gameStatus() {
+		return instance().gameStatus;
 	}
 
 }
